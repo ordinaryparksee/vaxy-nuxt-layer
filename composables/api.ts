4a5ef133b0f401ea844b45758eaa2d757cd587f5
vaxy <vaxy2@wetable.co.kr>
2023-10-12 23:40:25 +0900

    api

--- composables/api.ts
+++ composables/api.ts
@@ -5,14 +5,9 @@ import { AvailableRouterMethod, NitroFetchRequest } from 'nitropack'
 import { KeysOf, PickFrom } from '#app/composables/asyncData'
 
 export async function useApi<
-  ResT = void,
-  ErrorT = FetchError,
-  ReqT extends NitroFetchRequest = NitroFetchRequest,
-  Method extends AvailableRouterMethod<ReqT> = ResT extends void ? 'get' extends AvailableRouterMethod<ReqT> ? 'get' : AvailableRouterMethod<ReqT> : AvailableRouterMethod<ReqT>,
-  _ResT = ResT extends void ? FetchResult<ReqT, Method> : ResT,
-  DataT = _ResT, PickKeys extends KeysOf<DataT> = KeysOf<DataT>,
-  DefaultT = null
-> (url: string, opts?: UseFetchOptions<_ResT, DataT, PickKeys, DefaultT, ReqT, Method>): Promise<AsyncData<PickFrom<DataT, PickKeys> | DefaultT, ErrorT | null>> {
+  Data = void,
+  ErrorT = FetchError
+> (url: string, opts?: UseFetchOptions<Data>): Promise<AsyncData<PickFrom<Data, KeysOf<Data>> | null, ErrorT | null>> {
   const userAuth = useCookie('access_token')
   const baseURL = process.client ? '/api' : `${process.env.API_BASE_URL}/api`
   const headers: {[key: string]: string} = {
@@ -23,7 +18,7 @@ export async function useApi<
     headers.Authorization = `Bearer ${userAuth.value}`
   }
 
-  const defaults: UseFetchOptions<_ResT, DataT, PickKeys, DefaultT, ReqT, Method> = {
+  const defaults: UseFetchOptions<Data> = {
     baseURL: baseURL,
 
     // cache request
